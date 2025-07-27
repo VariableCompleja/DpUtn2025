6db3ce235a30e5f4eabce88549dfed73321dfa36
maria sol <variablecompleja@yahoo.com>
2025-07-27 18:12:11 -0300

    LoginLogout

--- back/routes/admin/login.js
+++ back/routes/admin/login.js
@@ -1,11 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('admin/login',{
-    layout:'admin/layout'
-  } );
+router.get('/', function (req, res, next) {
+  res.render('admin/login', { //va a buscar login.hbs
+    layout: 'admin/layout'
+  });
 });
+
+/*cierra el login*/
+router.get('/logout', function(req,res, next){
+  req.session.destroy();
+  res.render('admin/login', {
+    layout: 'admin/layout'
+  });
+});
+
+/*procesa el formulario */
+router.post('/', async (req, res, next) => {
+  try {
+    var usuario = req.body.usuario;
+    var password = req.body.password;
+    var data = await usuariosModel.getUserByUsernameAndPassword(usuario, password);
+
+    if (data != undefined) {
+      req.session.id_usuario = data.TId;
+      req.session.nombre = data.Tusuario;
+      res.redirect('/admin/novedades');
+    }
+    else {
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+);
 
 module.exports = router;
